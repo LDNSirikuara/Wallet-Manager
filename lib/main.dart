07acac988355b0e2a2ccc4bfd2df18858cb63853
Duminda Namal <dumindanamal@gmail.com>
2019-12-01 21:26:29 +0530

    Wallet Manager setup

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallet Manager',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manager'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.green,
+              child: Text('Chart!'),
+              elevation: 20,
+            ),
+          ),
+          Card(
+            child: Text('List of Tx'),
+            elevation: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
