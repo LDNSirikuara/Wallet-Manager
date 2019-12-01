24ca00e7710f643337c82f3b41b4799ab2a885bd
Duminda Namal <dumindanamal@gmail.com>
2019-12-02 02:52:17 +0530

    Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,32 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Bag',
+      amount: 2500,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Shoes',
+      amount: 4000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Rice',
+      amount: 250,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Pizza',
+      amount: 2900,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,12 +59,48 @@ class MyHomePage extends StatelessWidget {
               elevation: 20,
             ),
           ),
-          Card(
-            child: Text('List of Tx'),
-            elevation: 20,
+          Column(
+            children: transaction.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.blue,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(
+                          tx.date.toString(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
     );
   }
 }
+
